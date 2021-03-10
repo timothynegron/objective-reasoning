2c280d389511b7998b8aa256a2d8fa4c62e4a5c2
Timothy Negron <timothy.negron@codeimmersives.com>
2021-03-09 22:47:21 -0500

    Adds functions.

--- main.js
+++ main.js
@@ -2,11 +2,42 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+function isAdmin (user) {
+    return user.userRole === 'ADMIN' ? true : false;
+}
 
+function getEmail (user) {
+    return user.firstName.toLowerCase() + '.' + user.lastName.toLowerCase() + '@codeimmersives.com';
+}
+
+function getPlaylistLength (playlist) {
+    return playlist.songs.length;
+}
+
+function getHardestHomework (homeworks) {
+    let hardestHomeworkName = '';
+    let score = 101;
+    for (const homework of homeworks) {
+            if(homework.averageScore < score){
+                score = homework.averageScore;
+                hardestHomeworkName = homework.name;
+            }
+    }
+    return hardestHomeworkName;
+}
+
+function createPhonebook (people, phoneNumbers) {
+    let phoneBook = {};
+    for (let i = 0; i < people.length; i++){
+        phoneBook[people[i]] = phoneNumbers[i];
+    }
+    return phoneBook;
+}
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
 // └─────────────────────────────────────┘
+
 
 if (typeof isAdmin === 'undefined') {
     isAdmin = undefined;
